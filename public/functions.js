e1ef8a7aed5ef94f1387d21a4fd45fc3327c4fa5
Evelin Marques <56482367+evelinmarques@users.noreply.github.com>
2020-08-24 19:56:57 -0300

    Create functions.js

---
+++ public/functions.js
@@ -0,0 +1,23 @@
+function Soma(){
+    var n1 = prompt("Informe o primeiro numero:");
+    var n2 = prompt("Informe o segundo numero:");
+    alert(parseInt(n1)+parseInt(n2));
+}
+
+function Sub(){
+    var n1 = prompt("Informe o primeiro numero:");
+    var n2 = prompt("Informe o segundo numero:");
+    alert(parseInt(n1)-parseInt(n2));
+}
+
+function Div(){
+    var n1 = prompt("Informe o primeiro numero:");
+    var n2 = prompt("Informe o segundo numero:");
+    alert(parseInt(n1)/parseInt(n2));
+}
+
+function Multi(){
+    var n1 = prompt("Informe o primeiro numero:");
+    var n2 = prompt("Informe o segundo numero:");
+    alert(parseInt(n1)*parseInt(n2));
+}
